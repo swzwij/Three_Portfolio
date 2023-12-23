12ffbfb377d71e03691e1ea1e6b8ed8ac00b0b03
Samuel <70766878+swzwij@users.noreply.github.com>
2023-12-23 17:37:48 +0100

    cleaned up code

--- main.js
+++ main.js
@@ -1,23 +1,57 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-let camera, scene, renderer, light;
-let geometry, material, cube;
+let camera, scene, renderer;
+let cube;
 let controls;
 
 function init() 
 {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    camera.position.set(5, 3, 5);
+    setupCamera();
+    setupRenderer();
     
+    addLight();
+
+    addCube();
+    addPlane();
+
+    setupControls();
+
+    animate();
+}
+
+function setupCamera() 
+{
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(5, 3, 5);
+}
+
+function setupRenderer() 
+{
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
     document.body.appendChild(renderer.domElement);
-    
-    light = new THREE.DirectionalLight(0xffffff, 1);
+}
+
+function setupControls()
+{
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.copy(cube.position);
+
+    window.addEventListener('resize', () => 
+    {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    });
+}
+
+function addLight() 
+{
+    const light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(0, 5, 10);
     light.castShadow = true;
 
@@ -28,21 +62,13 @@ function init()
     light.shadow.camera.far = 50;
 
     scene.add(light);
+}
 
-    geometry = new THREE.BoxGeometry();
-    material = new THREE.MeshBasicMaterial({ color: 0x0059e8 });
-    material.receiveShadow = true;
-    cube = new THREE.Mesh(geometry, material);
-    cube.rotation.y = 45 * (Math.PI / 180);
-    cube.position.y = 0.5;
-    cube.castShadow = true;
-    cube.receiveShadow = true;
-
-    scene.add(cube);
-
-    const planeGeometry = new THREE.PlaneGeometry(10, 10); // Size of the plane
-    const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff }); // Material and color of the plane
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+function addPlane() 
+{
+    const geometry = new THREE.PlaneGeometry(10, 10);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const plane = new THREE.Mesh(geometry, material);
 
     plane.rotation.x = -Math.PI / 2;
     plane.position.y = 0;
@@ -50,18 +76,23 @@ function init()
     plane.receiveShadow = true;
 
     scene.add(plane);
-    
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.copy(cube.position);
-    
-    window.addEventListener('resize', () => 
-    {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    });
+}
 
-    animate();
+function addCube() 
+{
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x0059e8 });
+    material.receiveShadow = true;
+
+    cube = new THREE.Mesh(geometry, material);
+
+    cube.rotation.y = 45 * (Math.PI / 180);
+    cube.position.y = 0.5;
+
+    cube.castShadow = true;
+    cube.receiveShadow = true;
+
+    scene.add(cube);
 }
 
 function animate() 
