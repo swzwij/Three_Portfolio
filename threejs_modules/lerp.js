96310ffcb00232e3ee153ed560f7fa747e6cb55a
Samuel <70766878+swzwij@users.noreply.github.com>
2023-12-25 01:07:38 +0100

    made the lerp ease out and return when finished

--- threejs_modules/lerp.js
+++ threejs_modules/lerp.js
@@ -1,28 +1,46 @@
-export function lerpCamera(object, targetPosition, controls, targetControlsPosition, speed) {
+export function lerpCamera(object, targetPosition, controls, targetControlsPosition, speed) 
+{
     const startPosition = object.position.clone();
     const startControlsTarget = controls.target.clone();
 
     let delta = 0;
+    let animationFrameId;
 
-    function animate() {
-        requestAnimationFrame(animate);
+    function easeOutCubic(t) 
+    {
+        return 1 - Math.pow(1 - t, 3);
+    }
 
+    function animate() 
+    {
         delta += speed;
-        delta = Math.min(delta, 1); 
+        if (delta >= 1) 
+        {
+            delta = 1;
+            object.position.copy(targetPosition);
+            controls.target.copy(targetControlsPosition);
+
+            // Stop the animation
+            cancelAnimationFrame(animationFrameId);
+            return;
+        }
+
+        animationFrameId = requestAnimationFrame(animate);
+
+        const easedDelta = easeOutCubic(delta);
 
         // Lerp the object's position
-        object.position.copy(startPosition).lerp(targetPosition, delta);
+        object.position.copy(startPosition).lerp(targetPosition, easedDelta);
 
         // Lerp the controls' target
-        controls.target.copy(startControlsTarget).lerp(targetControlsPosition, delta);
+        controls.target.copy(startControlsTarget).lerp(targetControlsPosition, easedDelta);
 
-        // Update controls (important if controls are enabled)
+        // Update controls
         controls.update();
     }
 
-    animate();
+    animationFrameId = requestAnimationFrame(animate);
 }
-
 
 export function lerpLookAt(controls, target, speed)
 {
